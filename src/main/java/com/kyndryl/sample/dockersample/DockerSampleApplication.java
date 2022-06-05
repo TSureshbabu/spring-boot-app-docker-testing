7cec398b077ee4c9c007bf0aaef1e672b92e3b32
TSureshbabu <thotasuresh64@gmail.com>
2022-06-05 21:55:10 +0530

    Suresh made some change

--- src/main/java/com/kyndryl/sample/dockersample/DockerSampleApplication.java
+++ src/main/java/com/kyndryl/sample/dockersample/DockerSampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerSampleApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DockerSampleApplication.class, args);
 	}
 
